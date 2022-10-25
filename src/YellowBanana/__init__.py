d8c0f5ce3254412f605c0818eacd762577bf4c5f
anna-follestad-4ss <anna.fehn.follestad@4subsea.com>
2022-10-25 18:20:09 +0000

    Bump version: 0.1.17 → 0.1.18

--- src/YellowBanana/__init__.py
+++ src/YellowBanana/__init__.py
@@ -2,7 +2,7 @@ from YellowBanana._module1 import function1, function2
 from YellowBanana._module2 import function3, function4
 from YellowBanana._module3 import Class1
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 
 
 __all__ = ["function1", "function2", "function3", "function4", "Class1"]
